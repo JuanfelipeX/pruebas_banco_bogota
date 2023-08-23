151392bde50477c0fc0d0fcc555675e7a8353ac0
Juan Felipe Jimenez Pacheco <72418729+JuanfelipeX@users.noreply.github.com>
2023-08-23 11:08:34 -0500

    Update Problema_uno.java

--- Problema_uno.java
+++ Problema_uno.java
@@ -9,10 +9,14 @@ class Main {
         Map<Integer, Integer> c = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
+            // Contar la frecuencia del número en el mapa 'c'
+            // Si el número ya está en el mapa, aumentar su frecuencia en 1, de lo contrario, establecerlo en 1
             int num = scanner.nextInt();
             c.put(num, c.getOrDefault(num, 0) + 1);
         }
         int ans = 0;
+
+        // Calcular la respuesta final sumando la mitad de la frecuencia de cada número en el mapa 'c'
         for (int x : c.keySet()) {
             ans += c.get(x) / 2;
         }
@@ -20,5 +24,5 @@ class Main {
     }
 }
 
-//9 10 26 20 16 10 30 50 16 20
+//9 10 20 20 10 10 30 50 10 20
 //3
